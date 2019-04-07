066f70f2d7e0768e0da4e6de692a9f5c72d7c43e
avinanuami <avinanuami@gmail.com>
2019-04-07 23:56:32 +0530

    added http call, enabled sorting & filter feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AgGridNg2 } from 'ag-grid-angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  @ViewChild('agGrid') agGrid: AgGridNg2;
+
+  columnDefs = [
+    {headerName: 'Make', field: 'make', sortable: true, filter: true, checkboxSelection: true},
+    {headerName: 'Model', field: 'model', sortable: true, filter: true},
+    {headerName: 'Price', field: 'price', sortable: true, filter: true}
+  ];
+
+  rowData: any;
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit(): void {
+    this.rowData = this.http.get('https://api.myjson.com/bins/15psn9');
+  }
+
+  getSelectedRows() {
+    const selectedNodes = this.agGrid.api.getSelectedNodes();
+    const selectedData = selectedNodes.map( node => node.data);
+    const selectedDataStringPresentation = selectedData.map( node => node.make + ' ' + node.model).join(', ');
+    alert(`Selected nodes: ${selectedDataStringPresentation}`);
+  }
 }
